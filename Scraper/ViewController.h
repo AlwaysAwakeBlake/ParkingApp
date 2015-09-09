5e9d06a8d22258c78cf2b15d4ebaa8c646934fec
Blake Mabe <blakemabe@Blakes-MacBook-Pro.local>
2015-09-08 21:18:52 -0400

    Initial Commit

---
+++ Scraper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scraper
+//
+//  Created by Blake Mabe on 9/8/15.
+//  Copyright (c) 2015 BlakeMabeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
